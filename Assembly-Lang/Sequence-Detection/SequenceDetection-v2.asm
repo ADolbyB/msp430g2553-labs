8f7557f86edabbf7d6f9a6ddc5d192077608b965
Joel Brigida <joel@drtechreps.com>
2023-01-24 23:18:05 -0500

    Fix Code Formatting

--- Assembly-Lang/Sequence-Detection/SequenceDetection-v2.asm
+++ Assembly-Lang/Sequence-Detection/SequenceDetection-v2.asm
@@ -40,33 +40,33 @@ StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
 ; As soon as the value "1010xx1010" is found in the register as the stream of data is input, The program sets R5
 ; to "1111111111111111" aka 0xFFFF and then ends. This is an alternate version of the Sequence Detector v1.0
 ;
-START		mov.w	#0xA28A, R5				;examine some random value in R5
+START       mov.w   #0xA28A, R5             ; examine some random value in R5
 
-SEQ			push	R6						;save values in registers before routine starts
-			push	R10
-			push	R11
+SEQ         push    R6                      ; save values in registers before routine starts
+            push    R10
+            push    R11
 
- 			mov.b	#7, R10					;store # of iterations to RRC.W R5 (16 - 10 + 1)
+            mov.b   #7, R10                 ; store # of iterations to RRC.W R5 (16 - 10 + 1)
 
-LOOP		mov.w	R5, R6					;save R5 contents in R6 to be MASKED
-			and.w	#0x03CF, R6				;perform MASKING of the string pattern
-			cmp.w	#0x028A, R6				;SEARCH for String "1010xx1010" = 0x028A
-			jnz		SKIP					;SKIP if no match
+LOOP        mov.w   R5, R6                  ; save R5 contents in R6 to be MASKED
+            and.w   #0x03CF, R6             ; perform MASKING of the string pattern
+            cmp.w   #0x028A, R6             ; SEARCH for String "1010xx1010" = 0x028A
+            jnz     SKIP                    ; SKIP if no match
 
-SET_F		mov.w	#0xFFFF, R5				;set R5 to 0xFFFF if pattern is found in R6
-			jmp		END						;END the subroutine if found
+SET_F       mov.w   #0xFFFF, R5             ; set R5 to 0xFFFF if pattern is found in R6
+            jmp     END                     ; END the subroutine if found
 
-SKIP		rrc.w	R5						;Rotate Right the contents of R5 to be searched
-			dec.b	R10						;decrement counter
-			jnz		LOOP					;jump if not at end of search string
+SKIP        rrc.w   R5                      ; Rotate Right the contents of R5 to be searched
+            dec.b   R10                     ; decrement counter
+            jnz     LOOP                    ; jump if not at end of search string
 
-CLEAR		clr		R5						;clear R5 if pattern not found
+CLEAR       clr     R5                      ; clear R5 if pattern not found
 
-END			pop		R11						;restore original values of registers
-			pop		R10
-			pop		R6
+END         pop     R11                     ; restore original values of registers
+            pop     R10
+            pop     R6
 
-			ret								;return from subroutine
+            ret                             ; return from subroutine
 
 ;-------------------------------------------------------------------------------
 ; Stack Pointer definition
