6bebaea231407804abdeb5019a3913e70eb9e32e
Joel Brigida <joel@drtechreps.com>
2023-01-24 23:12:19 -0500

    Fix Code Formatting

--- Assembly-Lang/Sequence-Detection/SequenceDetection-v1.asm
+++ Assembly-Lang/Sequence-Detection/SequenceDetection-v1.asm
@@ -38,36 +38,36 @@ StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
 ; In this case, as soon as the pattern is found twice, the program sets R5 to 0xFFFF. This version
 ; requires a counter to count the instances of "1010xx1010" in the stream.
 ;
-START		mov.w	#0xA28A, R5				; examine this value in R5
+START       mov.w   #0xA28A, R5             ; examine this value in R5
 
-SEQ			push	R6						; save values in registers before routine starts
-			push	R10
-			push	R11
-			mov.b	#2, R11					; Upon 2 occurrences of 1010xx1010, SET R5 to 0xFFFF
+SEQ         push    R6                      ; save values in registers before routine starts
+            push    R10
+            push    R11
+            mov.b   #2, R11                 ; Upon 2 occurrences of 1010xx1010, SET R5 to 0xFFFF
 
- 			mov.b	#7, R10					; store # of iterations to RRC.W R5 (16 - 10 + 1)
+            mov.b   #7, R10                 ; store # of iterations to RRC.W R5 (16 - 10 + 1)
 
-LOOP		mov.w	R5, R6					; save R5 contents in R6 to be MASKED
-			and.w	#0x03CF, R6				; perform MASKING of the string pattern
-			cmp.w	#0x028A, R6				; SEARCH for String "1010xx1010" = 0x028A
-			jnz		SKIP					; SKIP if no match
-			dec.b	R11						; decrement R11 if match is found
-			jz		SET_F					; set R5 to 0xFFFF if pattern is found twice
+LOOP        mov.w   R5, R6                  ; save R5 contents in R6 to be MASKED
+            and.w   #0x03CF, R6             ; perform MASKING of the string pattern
+            cmp.w   #0x028A, R6             ; SEARCH for String "1010xx1010" = 0x028A
+            jnz     SKIP                    ; SKIP if no match
+            dec.b   R11                     ; decrement R11 if match is found
+            jz      SET_F                   ; set R5 to 0xFFFF if pattern is found twice
 
-SKIP		rrc.w	R5						; Rotate Right the contents of R5 to be searched
-			dec.b	R10						; decrement counter
-			jz		CLEAR					; Clear R5 if pattern not found
-			jnz		LOOP					; jump if not at end of search string
+SKIP        rrc.w   R5                      ; Rotate Right the contents of R5 to be searched
+            dec.b   R10                     ; decrement counter
+            jz      CLEAR                   ; Clear R5 if pattern not found
+            jnz     LOOP                    ; jump if not at end of search string
 
-CLEAR		clr		R5						; clear R5 if pattern not found
-			jmp		END						; jump to end
+CLEAR       clr     R5                      ; clear R5 if pattern not found
+            jmp     END                     ; jump to end
 
-SET_F		mov.w	#0xFFFF, R5				; set R5 to 0xFFFF if pattern is found twice
+SET_F       mov.w   #0xFFFF, R5             ; set R5 to 0xFFFF if pattern is found twice
 
-END			pop		R11						; restore original values of registers
-			pop		R10
-			pop		R6
-			ret								; return from subroutine
+END         pop     R11                     ; restore original values of registers
+            pop     R10
+            pop     R6
+            ret                             ; return from subroutine
 
 ;-------------------------------------------------------------------------------
 ; Stack Pointer definition
