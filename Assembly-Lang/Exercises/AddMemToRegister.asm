dcc58af4e32a0fce09f081a92593a8167eeef181
Joel Brigida <joel@drtechreps.com>
2023-01-25 23:49:30 -0500

    Fix Code Formatting

--- Assembly-Lang/Exercises/AddMemToRegister.asm
+++ Assembly-Lang/Exercises/AddMemToRegister.asm
@@ -27,26 +27,26 @@ StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
 ;-------------------------------------------------------------------------------
 ; Main loop here
 ;-------------------------------------------------------------------------------
-LAB0		mov.w   #01,&0200h      ; set a number on location $0200
-			mov.w   #02,&0202h      ; set a number on location $0202
-			mov.w   #03,&0204h      ; set a number on location $0204
+LAB0        mov.w   #01,&0200h              ; set a number on location $0200
+            mov.w   #02,&0202h              ; set a number on location $0202
+            mov.w   #03,&0204h              ; set a number on location $0204
 
-LINEA		clr		R7				; clear the entire R7 register
-			clr		R8				; clear the entire R8 register
-			clr		R9				; clear the entire R9 register
-			clr		R10				; clear the entire R10 register
+LINEA       clr     R7                      ; clear the entire R7 register
+            clr     R8                      ; clear the entire R8 register
+            clr     R9                      ; clear the entire R9 register
+            clr     R10                     ; clear the entire R10 register
 
-LINEB       mov.w	&0200h, R7		; copy a word from &0200h to R7
-            mov.w	&0202h, R8		; copy a word from &0202h to R8
-            mov.w	&0204h, R9		; copy a word from &0204h to R9
+LINEB       mov.w   &0200h, R7              ; copy a word from &0200h to R7
+            mov.w   &0202h, R8              ; copy a word from &0202h to R8
+            mov.w   &0204h, R9              ; copy a word from &0204h to R9
 
 
-LINEC       mov.b	R7, R10			; Start accumulatior R10 with R7 value
-            add.b	R8, R10			; add to it the content R8
-            add.b	R9, R10			; add to it the content R9
-            mov.w	R10,&0206h		; now store the sum in L
+LINEC       mov.b   R7, R10                 ; Start accumulatior R10 with R7 value
+            add.b   R8, R10                 ; add to it the content R8
+            add.b	R9, R10                 ; add to it the content R9
+            mov.w   R10,&0206h              ; now store the sum in L
 
-Mainloop	jmp     Mainloop        ; Loop in Place when done.
+Mainloop    jmp     Mainloop                ; Loop in Place when done.
 ;-------------------------------------------------------------------------------
 ; Stack Pointer definition
 ;-------------------------------------------------------------------------------
