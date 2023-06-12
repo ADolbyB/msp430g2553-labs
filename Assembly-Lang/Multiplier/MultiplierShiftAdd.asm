a2f43db26dad9a98077ca424b5d76b7295394f36
Joel Brigida <joel@drtechreps.com>
2023-06-11 23:03:39 -0400

    Shift / Add Multiplier in ASM

--- Assembly-Lang/Multiplier/MultiplierShiftAdd.asm
+++ Assembly-Lang/Multiplier/MultiplierShiftAdd.asm
@@ -29,24 +29,24 @@ StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
 loop        jmp     loop                    ; loop in place
 
 MUL:        push.w  R5                      ; Push current values to the stack
-			push.w  R7                      ;
-			push.w  R8                      ;
-			mov.w   #8, R8                  ; 8 bit multiplication, so we loop 8 times
-			clr.w   R7                      ; additive accumulator should start with zero
-			and.w	#0x00FF, R5             ; clear upper 8 bits of mltiplier
-			and.w 	#0x00FF, R6             ; clear upper 8 bits of multipliant
+            push.w  R7                      ;
+            push.w  R8                      ;
+            mov.w   #8, R8                  ; 8 bit multiplication, so we loop 8 times
+            clr.w   R7                      ; additive accumulator should start with zero
+            and.w   #0x00FF, R5             ; clear upper 8 bits of mltiplier
+            and.w   #0x00FF, R6             ; clear upper 8 bits of multipliant
 
 nextbit	    rrc.w   R5                      ; shift multiplier bits one at a time to the carry
 			jnc     twice                   ; if no carry skip the add
 
-addmore		add.w   R6, R7                  ; add a copy of the multiplicand to the accumulator
+addmore     add.w   R6, R7                  ; add a copy of the multiplicand to the accumulator
 twice		add.w   R6, R6                  ; multiplicand times 2, (shifted 1 bit left)
-			dec.w	R8			            ; decrement loop counter
-			jnz     nextbit		            ; jump to check next bit of the multiplier
-			mov.w   R7, R6                  ; save the result in R6
+            dec.w   R8			            ; decrement loop counter
+            jnz     nextbit		            ; jump to check next bit of the multiplier
+            mov.w   R7, R6                  ; save the result in R6
 
-			pop.w   R8                      ; Restore previous values to the stack
-			pop.w   R7                      ;
+            pop.w   R8                      ; Restore previous values to the stack
+            pop.w   R7                      ;
             pop.w   R5                      ;
 
 MULTend     ret                             ; Return from subroutine
