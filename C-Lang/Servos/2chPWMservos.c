55df96a1f94d4bac8644779d92aaf6b5bfe5eb77
Joel Brigida <joel@drtechreps.com>
2023-07-07 16:39:15 -0400

    Fix Comment Alignment

--- C-Lang/Servos/2chPWMservos.c
+++ C-Lang/Servos/2chPWMservos.c
@@ -13,40 +13,40 @@
 #include <stdlib.h>
 
 unsigned int ADCReading[2];
-double potvals[2];                                              // potentiometer values
+double potvals[2];                                                  // potentiometer values
 double prevals[2];
 
-void configureADC()                                             // set up the ADC
+void configureADC()                                                 // set up the ADC
 {
-    ADC10CTL1 = (INCH_2 | CONSEQ_1);                		    // channels A0 to A2, single sequence
-    ADC10CTL0 = (ADC10SHT_2 | MSC | ADC10ON | SREF_0);          // 16 clocks, multiple sample cov, ADC on, Ver GND-VCC
-    ADC10DTC1 = 0x03;                             		        // 3 conversions
-    ADC10AE0 |= (BIT1 | BIT2);                    		        // ADC10 option select
-    ADC10SA = (unsigned)&ADCReading[0]; 				        // RAM Address of ADC Data, must be reset every conversion
+    ADC10CTL1 = (INCH_2 | CONSEQ_1);                                // channels A0 to A2, single sequence
+    ADC10CTL0 = (ADC10SHT_2 | MSC | ADC10ON | SREF_0);              // 16 clocks, multiple sample cov, ADC on, Ver GND-VCC
+    ADC10DTC1 = 0x03;                                               // 3 conversions
+    ADC10AE0 |= (BIT1 | BIT2);                                      // ADC10 option select
+    ADC10SA = (unsigned)&ADCReading[0];                             // RAM Address of ADC Data, must be reset every conversion
 }
 
-void readPot()                                                  // read values from analog input joysticks
+void readPot()                                                      // read values from analog input joysticks
 {
     int i = 0, j = 10;
-    for (i = 0; i < j ; ++i)                 			        // read all three analog values j nmber of times
+    for (i = 0; i < j ; ++i)                                        // read all three analog values j nmber of times
     {
-        ADC10SA = (unsigned)&ADCReading[0]; 			        // RAM Address of ADC Data, must be reset every conversion
-        ADC10CTL0 |= (ENC | ADC10SC);          			        // Start ADC Conversion
+        ADC10SA = (unsigned)&ADCReading[0];                         // RAM Address of ADC Data, must be reset every conversion
+        ADC10CTL0 |= (ENC | ADC10SC);                               // Start ADC Conversion
         
-        while (ADC10CTL1 & BUSY);              			        // Wait while ADC is busy
+        while (ADC10CTL1 & BUSY);                                   // Wait while ADC is busy
         
         potvals[0] += ADCReading[0];
         potvals[1] += ADCReading[1];
     }
     
     potvals[0] /= j;
-    potvals[1] /= j;                                            // Average the readings for the three variables
+    potvals[1] /= j;                                                // Average the readings for the three variables
     __delay_cycles(100);
 }
 
-void setServos0()                                               // activate servo 0
+void setServos0()                                                   // activate servo 0
 {
-    potvals[0] = ((potvals[0] - (677 - 512)) * 100) / (1024);   // Normalize pot values
+    potvals[0] = ((potvals[0] - (677 - 512)) * 100) / (1024);       // Normalize pot values
     
     if(potvals[0] <= 1)
     {
@@ -57,16 +57,16 @@ void setServos0()                                               // activate serv
         potvals[0] = 100;
     }
 
-    TA0CCR0 = 10000;                                            // Total PWM period, 10000 microsecond
-    TA0CCR1 = 7500 + potvals[0] * 20;                           // Range of PWM off portion between 7500-9500, making duty cycle 2500-500 microsecond
-    TA0CCTL1 = OUTMOD_3;                                        // PWM output mode: 3 - PWM set/reset
-    TA0CTL = TASSEL_2 + MC_1;                                   // SMCLK, Up to CCR0
+    TA0CCR0 = 10000;                                                // Total PWM period, 10000 microsecond
+    TA0CCR1 = 7500 + potvals[0] * 20;                               // Range of PWM off portion between 7500-9500, making duty cycle 2500-500 microsecond
+    TA0CCTL1 = OUTMOD_3;                                            // PWM output mode: 3 - PWM set/reset
+    TA0CTL = TASSEL_2 + MC_1;                                       // SMCLK, Up to CCR0
     __delay_cycles(100);
 }
 
-void setServos1()                                               // activate servo 1
+void setServos1()                                                   // activate servo 1
 {
-    potvals[1] = ((potvals[1] - (580 - 512)) * 100) / (1024);   // Normalize the pot values
+    potvals[1] = ((potvals[1] - (580 - 512)) * 100) / (1024);       // Normalize the pot values
     
     if(potvals[1] <= 1)
     {
@@ -77,19 +77,19 @@ void setServos1()                                               // activate serv
         potvals[1] = 100;
     }
 
-    TA1CCR0 = 10000;                                            // Total PWM period, 10000 microsecond
-    TA1CCR1 = 7500 + potvals[1] * 20;                           // Range of PWM off portion between 7500-9500, making duty cycle 2500-500 microsecond
-    TA1CCTL1 = OUTMOD_3;                                        // PWM output mode: 3 - PWM set/reset
-    TA1CTL = TASSEL_2 + MC_1;                                   // SMCLK, Up to CCR0
+    TA1CCR0 = 10000;                                                // Total PWM period, 10000 microsecond
+    TA1CCR1 = 7500 + potvals[1] * 20;                               // Range of PWM off portion between 7500-9500, making duty cycle 2500-500 microsecond
+    TA1CCTL1 = OUTMOD_3;                                            // PWM output mode: 3 - PWM set/reset
+    TA1CTL = TASSEL_2 + MC_1;                                       // SMCLK, Up to CCR0
     __delay_cycles(100);
 }                                
 
 int main(void)
 {
-    WDTCTL = WDTPW | WDTHOLD;                                   // Deactivate watchdog timer
+    WDTCTL = WDTPW | WDTHOLD;                                       // Deactivate watchdog timer
     P1DIR = 0;
     P2DIR = 0;
-    P1DIR |= BIT6;                                              // P1.6, 2.1 are outputs to the servos
+    P1DIR |= BIT6;                                                  // P1.6, 2.1 are outputs to the servos
     P2DIR |= BIT1;
     P1SEL |= BIT6;
     P2SEL |= BIT1;
@@ -130,7 +130,7 @@ int main(void)
     }
 }
 
-#pragma vector = ADC10_VECTOR                                   // ISR Routine
+#pragma vector = ADC10_VECTOR                                       // ISR Routine
 __interrupt void ADC10_ISR(void)
 {
     __bic_SR_register_on_exit(CPUOFF);
