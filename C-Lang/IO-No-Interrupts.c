70a5236c8b08c4e2c34c225a3a02327a7564a1c5
Joel Brigida <joel@drtechreps.com>
2023-07-08 12:31:51 -0400

    Fix Comments & Parentheses

--- C-Lang/IO-No-Interrupts.c
+++ C-Lang/IO-No-Interrupts.c
@@ -21,21 +21,21 @@ int main(void)
     P1OUT |= BIT3;                                  // activate pull up resistor: SW2 is active low (Hardware on Board)
 
     for(;;)
-        {
-            sw2 = P1IN;                             // read values from P1
-            sw2 &= BIT3;                            // mask out only BIT3 where SW2 is connected
+    {
+        sw2 = P1IN;                                 // read values from P1
+        sw2 &= BIT3;                                // mask out only BIT3 where SW2 is connected
 
-            if (sw2 == BIT3)                        // BIT3 = 0x08: if SW2 is high
-                {
-                    P1OUT &= ~BIT6;                 // Bitwise AND: turn LED2 off
-                    P1OUT ^= BIT0;                  // Bitwise XOR toggles LED1 to next state on next iteration of the loop
-                    __delay_cycles(100000);         // delay in microseconds
-                }
-            else                                    // else (if SW2 is low)
-                {
-                    P1OUT &= ~BIT0;                 // Bitwise AND: turn LED1 off
-                    P1OUT ^= BIT6;                  // Bitwise XOR: toggles LED2 to next state on next iteration of the loop
-                    __delay_cycles(100000);         // delay in microseconds
-                }
-        }   // end infinite loop
-}// end main
+        if (sw2 == BIT3)                            // BIT3 = 0x08: if SW2 is high
+        {
+            P1OUT &= ~BIT6;                         // Bitwise AND: turn LED2 off
+            P1OUT ^= BIT0;                          // Bitwise XOR toggles LED1 to next state on next iteration of the loop
+            __delay_cycles(100000);                 // delay in microseconds
+        }
+        else                                    // else (if SW2 is low)
+        {
+            P1OUT &= ~BIT0;                 // Bitwise AND: turn LED1 off
+            P1OUT ^= BIT6;                  // Bitwise XOR: toggles LED2 to next state on next iteration of the loop
+            __delay_cycles(100000);         // delay in microseconds
+        }
+    }   // END for(;;)
+}// end main()
