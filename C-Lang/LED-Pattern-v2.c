1029781cab75298c61e54253b8b2244a3df9a5b3
Joel Brigida <joel@drtechreps.com>
2023-07-09 23:39:40 -0400

    Fix Comments & Parentheses

--- C-Lang/LED-Pattern-v2.c
+++ C-Lang/LED-Pattern-v2.c
@@ -13,58 +13,57 @@ and slow speed toggled by the second switch.
 
 int main(void)
 {
-    WDTCTL = WDTPW | WDTHOLD; // Stop watch dog timer
+    WDTCTL = WDTPW | WDTHOLD;                                           // Stop watch dog timer
 
     int pattern = 1;
     int R5_SW = 0, R6_LED = 0;
 
-    P1OUT = 0b00000000; // ZERO the OUTPUT upon Power Up
-    P1DIR = 0b11111111; // Set P1 to OUTPUT
-    P2DIR = 0b00000000; // Set P2 to INPUT
-    P2REN = 0xFF;       // enable all resistors on port 2 for Debounce Switches
-    P2OUT = 0xFF;       // make them all of them pull ups
+    P1OUT = 0b00000000;                                                 // ZERO the OUTPUT upon Power Up
+    P1DIR = 0b11111111;                                                 // Set P1 to OUTPUT
+    P2DIR = 0b00000000;                                                 // Set P2 to INPUT
+    P2REN = 0xFF;                                                       // enable all resistors on port 2 for Debounce Switches
+    P2OUT = 0xFF;                                                       // make them all of them pull ups
 
     while (1)
+    {
+        R5_SW = P2IN;                                                   // read the states of the switches
+
+        // check P2.0 for read mode
+        if (R5_SW & BIT0)                                               // MASKS the entire switch sequence and checks which bits are high
         {
-            // read the states of the switches
-            R5_SW = P2IN;
+            R6_LED = R5_SW & (BIT3 | BIT4 | BIT5);                      // mask pattern bits: which LEDs are toggled separately
+            P1OUT = R6_LED;                                             // set display pattern
+        }
 
-            // check P2.0 for read mode
-            if (R5_SW & BIT0) // MASKS the entire switch sequence and checks which bits are high
+        else                                                            // Rotate & Display mode
+        {
+            if(R5_SW & BIT1)
+            {
+                P1OUT = pattern;
+                pattern = pattern >> 1;
+                if(pattern == 0)
                 {
-                    // mask pattern bits: which LEDs are toggled separately
-                    R6_LED = R5_SW & (BIT3 | BIT4 | BIT5);
-                    P1OUT = R6_LED; // set display pattern
+                    pattern = 128;
                 }
+            }
+            else
+            {
+                P1OUT = pattern;
+                pattern = pattern << 1;
+                if(pattern > 255)
+                {
+                    pattern = 1;
+                }
+            }
 
-            else // Rotate & Display mode
-                {
-                    if(R5_SW & BIT1)
-                        {
-                            P1OUT = pattern;
-                            pattern = pattern >> 1;
-                            if(pattern == 0)
-                                {
-                                    pattern = 128;
-                                }
-                        }
-                    else
-                        {
-                            P1OUT = pattern;
-                            pattern = pattern << 1;
-                            if(pattern > 255)
-                                {
-                                    pattern = 1;
-                                }
-                        }
-                    if(R5_SW & BIT2)
-                        {
-                            __delay_cycles(50000); // fast
-                        }
-                    else
-                        {
-                            __delay_cycles(150000); // slow
-                        }
-                }
-        } //end while
-} // end main
+            if(R5_SW & BIT2)
+            {
+                __delay_cycles(50000);                                  // fast
+            }
+            else
+            {
+                __delay_cycles(150000);                                 // slow
+            }
+        }
+    }   //end while
+}   // end main
