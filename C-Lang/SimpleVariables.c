367852a898e27474a41d1ac311f1b8458fd613cc
Joel Brigida <joel@drtechreps.com>
2023-07-10 12:35:11 -0400

    Fix Comments & Parentheses

--- C-Lang/SimpleVariables.c
+++ C-Lang/SimpleVariables.c
@@ -6,19 +6,19 @@
 
 #include <msp430.h> 
 
-int   a = 10;								// 16 bit number
-long int b = 0;								// 32 bit number
-char c = 'c';								// 16 bit, 0x63 in ASCII
-float d = -255.25;							// 64 bit number
+int a = 10;                                 // 16 bit number
+long int b = 0;                             // 32 bit number
+char c = 'c';                               // 16 bit, 0x63 in ASCII
+float d = -255.25;                          // 64 bit number
 
 int main(void)
 {
-    WDTCTL = WDTPW | WDTHOLD;				// Stop watchdog timer
+    WDTCTL = WDTPW | WDTHOLD;               // Stop watchdog timer
     P1DIR |= BIT0;
 	
 	for(;;)
 	{
-		b = b + 1;							// increment variable
-		P1OUT ^= BIT0;						// Toggle the LED on BIT0
+		b = b + 1;                          // increment variable
+		P1OUT ^= BIT0;                      // Toggle the LED on BIT0
 	}
 }
