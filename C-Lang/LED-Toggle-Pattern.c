173b98126673f688bdd875c4608955014a265b17
Joel Brigida <joel@drtechreps.com>
2023-07-10 12:07:34 -0400

    Fix Comments & Parenthesis

--- C-Lang/LED-Toggle-Pattern.c
+++ C-Lang/LED-Toggle-Pattern.c
@@ -15,42 +15,40 @@ in the chosen pattern.
 
 int main(void)
 {
-    WDTCTL = WDTPW | WDTHOLD;    // Stop watch dog timer
+    WDTCTL = WDTPW | WDTHOLD;                                   // Stop watch dog timer
 
     int R5_SW = 0, R6_LED = 0;
 
-    P1OUT = 0b00000000;         // P1OUT = 0x00
-    P1DIR = 0b11111111;         // P1DIR = 0xFF
-    P2DIR = 0b00000000;         // P2DIR = 0x00
-    P2REN = 0xFF;               // enable all resistors on port 2
-    P2OUT = 0xFF;               // make them all of them pull ups
+    P1OUT = 0b00000000;                                         // P1OUT = 0x00
+    P1DIR = 0b11111111;                                         // P1DIR = 0xFF
+    P2DIR = 0b00000000;                                         // P2DIR = 0x00
+    P2REN = 0xFF;                                               // enable all resistors on port 2
+    P2OUT = 0xFF;                                               // make them all of them pull ups
 
     while (1)
+    {
+        R5_SW = P2IN;                                           // read switch states
+
+        // checking P2.0 for read mode
+        if (R5_SW & BIT0)                                       // MASKS the entire switch sequence
         {
-            // read switch states
-            R5_SW = P2IN;
+            R6_LED = R5_SW & (BIT2 | BIT3 | BIT4 | BIT5);       // mask pattern bits: which LEDs are toggled separately
+            P1OUT = R6_LED;                                     // sets the display pattern
+        }
+        else                                                    // display the pattern
+        {
+            R6_LED ^= 0xFF;                                     // toggle pattern by XORing w/ all 1's
+            R6_LED &= 0x00FF;                                   // mask any excessive bits
+            P1OUT = R6_LED;                                     // Displays the pattern
+        }
 
-            // checking P2.0 for read mode
-            if (R5_SW & BIT0) // MASKS the entire switch sequence
-                {
-                    // mask pattern bits: which LEDs are toggled separately
-                    R6_LED = R5_SW & (BIT2 | BIT3 | BIT4 | BIT5);
-                    P1OUT = R6_LED; // sets the display pattern
-                }
-
-            else // display the pattern
-                {
-                    R6_LED ^= 0xFF;     // toggle pattern by XORing w/ all 1's
-                    R6_LED &= 0x00FF;   // mask any excessive bits
-                    P1OUT = R6_LED;     // Displays the pattern
-                }
-            if (R5_SW & BIT1)
-                {
-                    __delay_cycles(120000); // fast
-                }
-            else
-                {
-                    __delay_cycles(175000); // slow
-                }
-        }   //end while
+        if (R5_SW & BIT1)
+        {
+            __delay_cycles(120000);                             // fast
+        }
+        else
+        {
+            __delay_cycles(175000);                             // slow
+        }
+    }   //end while
 }   // end main
