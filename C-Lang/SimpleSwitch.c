0da34e87635a784d8ef3596bed8cd97cd8e495b3
Joel Brigida <joel@drtechreps.com>
2023-07-10 12:29:17 -0400

    Fix Comments & Parentheses

--- C-Lang/SimpleSwitch.c
+++ C-Lang/SimpleSwitch.c
@@ -11,17 +11,17 @@ int sw2 = 0;                                            // declaration for switc
 
 int main(void)
 {
-    WDTCTL = WDTPW | WDTHOLD;	                        // Stop watchdog timer
+    WDTCTL = WDTPW | WDTHOLD;                           // Stop watchdog timer
 
-    P1DIR  = 0x00;				                        // Port 1 all inputs
+    P1DIR  = 0x00;                                      // Port 1 all inputs
     P1DIR  = (BIT0 | BIT6);                             // Port 1.0 and 1.6 out pututs for the LEDs
-    P1REN |= BIT3;				                        // Activate resister on P1.3
-    P1OUT |= BIT3;				                        // Make it pull up. becuase SW1 asserts a 0
+    P1REN |= BIT3;                                      // Activate resister on P1.3
+    P1OUT |= BIT3;                                      // Make it pull up. becuase SW1 asserts a 0
 
     for(;;)
     { 
-        sw2 = P1IN;				                        // read complete Port1
-        sw2 &= BIT3;				                    // mask out only BIT 3 where SW1 is connected
+        sw2 = P1IN;                                     // read complete Port1
+        sw2 &= BIT3;                                    // mask out only BIT 3 where SW1 is connected
         
         if (sw2 == BIT3)                                // if SW2 is high toggle BIT 0 which is LED1
         {
@@ -29,7 +29,7 @@ int main(void)
             P1OUT ^= BIT0;
             __delay_cycles(50000);
         }
-        else						                    // if SW2 is low toggle BIT 6 which is LED2
+        else                                            // if SW2 is low toggle BIT 6 which is LED2
         {
             P1OUT &= ~BIT0;
             P1OUT ^= BIT6;
