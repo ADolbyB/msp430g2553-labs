6313b5473bded48155d43050e045e58942773dd5
Joel Brigida <joel@drtechreps.com>
2023-07-10 12:31:09 -0400

    Fix Comments & Parentheses

--- C-Lang/SimpleVariables.c
+++ C-Lang/SimpleVariables.c
@@ -6,14 +6,14 @@
 
 #include <msp430.h> 
 
-int   a = 10;         						// 16 bit number
-long int b = 0;     						// 32 bit number
-char c = 'c';        						// 16 bit, 0x63 in ASCII
-float d = -255.25;                          // 64 bit number
+int   a = 10;								// 16 bit number
+long int b = 0;								// 32 bit number
+char c = 'c';								// 16 bit, 0x63 in ASCII
+float d = -255.25;							// 64 bit number
 
 int main(void)
 {
-    WDTCTL = WDTPW | WDTHOLD;  				// Stop watchdog timer
+    WDTCTL = WDTPW | WDTHOLD;				// Stop watchdog timer
     P1DIR |= BIT0;
 	
 	for(;;)
